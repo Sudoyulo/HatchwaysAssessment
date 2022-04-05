df5b28286ea66d6da8f9281c1765b5c06c31b3d0
Kevin Tan Yulo <yulokevin@gmail.com>
2022-04-05 13:56:14 -0700

    comments added

--- example-app/src/components/Assessment.jsx
+++ example-app/src/components/Assessment.jsx
@@ -7,9 +7,9 @@ import Tags from "./Tags";
 
 const Assessment = () => {
 
-  const [data, setdata] = useState([])
-  const [filter, setfilter] = useState([])
-  const [expanded, setExpanded] = useState([0])
+  const [data, setdata] = useState([]) //initial data from API
+  const [filter, setfilter] = useState([]) //used for search filter
+  const [expanded, setExpanded] = useState([0]) //used for expanded test scores
 
   const getInfo = () => {
     axios.get("https://api.hatchways.io/assessment/students")
@@ -18,12 +18,12 @@ const Assessment = () => {
         list.forEach((student) => {
           student.tag = []
         })
-        setdata(list)
-        setfilter(list)
+        setdata(list) //set intial
+        setfilter(list) //set manupilative
       })
   }
 
-  const calculateAverage = (grades) => {
+  const calculateAverage = (grades) => { //return average
     let sum = 0;
     grades.forEach((grade) => { sum += Number(grade) })
     return sum / grades.length;
@@ -31,22 +31,21 @@ const Assessment = () => {
 
   const studentList = filter.map((student, index) => {
 
-    const toggleClass = (id) => {
+    const toggleClass = (id) => { //Average Test Expander
       if (expanded.includes(id)) {
-        setExpanded(expanded.filter((stu) => { return stu !== id }))
+        setExpanded(expanded.filter((stu) => { return stu !== id })) //remove from expanded scored
         student.show = !student.show;
       } else {
-        setExpanded([...expanded].concat([id]))
+        setExpanded([...expanded].concat([id])) //add to list of expanded test scores
         student.show = !student.show;
       }
     }
 
-    let average = Math.round(calculateAverage(student.grades) * 100) / 100;
+    const average = Math.round(calculateAverage(student.grades) * 100) / 100; //2 sig fig
 
     return (
       <div className="student" key={index}>
         <div className="student-body">
-
           <div className="profile-picture">
             <img className="avatar" src={student.pic} alt="profile" />
           </div>
@@ -69,32 +68,30 @@ const Assessment = () => {
     )
   })
 
-  const searchbar = (key) => {
+  const searchbar = (key) => { //uses original data to filter names. does not work with tags
     setfilter([...data].filter((student) => {
-      if (student.firstName.toUpperCase().includes(key) || student.lastName.toUpperCase().includes(key)) { return student; }
+      if (student.firstName.toLowerCase().includes(key) || student.lastName.toLowerCase().includes(key)) { return student; }
     }))
   }
 
-  const searchtag = (key) => {
+  const searchtag = (key) => { //uses original data to filter names. does not work with search
     setfilter([...data].filter((student) => {
       let found = false;
-      student.tag.forEach((tag) => {
-        if (tag.toLowerCase().includes(key)) { found = true }
-      })
+      student.tag.forEach((tag) => { if (tag.toLowerCase().includes(key)) { found = true } })
       if (found) { return student }
     }))
 
-    if (key === "") { setfilter(data) }
+    if (key === "") { setfilter(data) } //reset when blank
   }
 
-  useEffect(() => {
+  useEffect(() => { //load async data on page load
     getInfo();
   }, [])
 
   return (
     <div id="information">
       <div id="header">
-        <input className="search" type="text" placeholder="Search by name" onChange={(e) => { searchbar(e.target.value.toUpperCase()) }} />
+        <input className="search" type="text" placeholder="Search by name" onChange={(e) => { searchbar(e.target.value.toLowerCase()) }} />
         <input className="search" type="text" placeholder="Search by tag" onChange={(e) => { searchtag(e.target.value.toLowerCase()) }} />
       </div>
       <div className="list">
